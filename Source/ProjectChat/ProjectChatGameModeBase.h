d1d6131f01b8c6cf86502d722cfd20ec2288170d
beomgeunchoi <o7716@naver.com>
2022-03-03 18:16:33 +0900

    Init Project Setting

---
+++ Source/ProjectChat/ProjectChatGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectChatGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTCHAT_API AProjectChatGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
